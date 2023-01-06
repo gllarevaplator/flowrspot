be93f0bbc0cccf8493adb7ca6afcbdc1d6a3b20a
platorgllareva <gllarevap@gmail.com>
2023-01-06 16:29:39 +0100

    Footer Added to the app

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer: React.FC = () => {
   return (
     <div className="text-center bg-dark text-white p-4 mt-4">
-      Flowrspot App © - Plator Gllareva
+      Flowrspot App - Plator Gllareva ©
     </div>
   );
 };
