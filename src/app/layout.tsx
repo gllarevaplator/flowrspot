bc491dad098b5adf494ccaa085174e1b95d94d42
pixelated-chilies <info@pixelatedchilies.com>
2025-03-17 12:20:00 +0100

    Add tailwindcss, postcss, remove bootstrap & optimize fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,31 @@ import { Provider } from "react-redux";
 import store from "../features/app/store";
 import Head from "next/head";
 import { Metadata } from "next";
+import { Montserrat, Playfair } from "next/font/google";
+import { Ubuntu } from "next/font/google";
+import "@/styles/globals.css";
 
 export const meta: Metadata = {
   title: "Flowers App",
   description:
     "Discover flowers around you, explore between more than 8.427 sightings",
 };
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+const ubuntu = Ubuntu({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+const playFair = Playfair({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
@@ -22,35 +41,13 @@ export default function RootLayout({
         <meta charSet="utf-8" />
         <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin=""
-        />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin=""
-        />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin=""
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-          rel="stylesheet"
-        />
       </Head>
-      <body>
+      <body
+        className={`${montserrat.className} ${ubuntu.className} ${playFair.className}`}
+      >
         <Provider store={store}>
-          <div id="root">{children}</div>
+          <main id="root">{children}</main>
         </Provider>
-        <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
-        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
       </body>
     </html>
   );
